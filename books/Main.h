db7cd40d820032ad15e49ef2da4f1186ddab06fd
Lyng308 <51954056+Lyng308@users.noreply.github.com>
2020-01-23 09:55:46 +0100

    Add files via upload

---
+++ books/Main.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <stdio.h> /* This is the include file for handling
+					  standard input & output
+					  2017 August PEFA                      */
+#include <ctype.h>
+#include <conio.h>
+#include <locale.h>
+
+#define MAXBOOKS	100
+
+#define MAXTITLE	50
+#define MAXAUTHOR	50
+#define MAXSUBJECT	100
+
+struct Books {
+	char  title[MAXTITLE];
+	char  author[MAXAUTHOR];
+	char  subject[MAXSUBJECT];
+	float book_price;
+};
+
+void printBook(struct Books *book);
+void menu(void);
+
+void AddBook(struct Books *book);
+
+void printAllBooks(struct Books *library);
+
+void DeleteBook(struct Books *book);
---
+++ faber/Main.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <stdio.h> /* This is the include file for handling
+					  standard input & output 
+					  2017 August PEFA                      */
